664b559b40c499783a6fbc06574a7f8f3e73b685
Jose I. Padilla <jpadil02@baker.edu>
2019-04-07 22:01:30 -0400

    windsow app24

---
+++ WindowsApp24/WindowsApp24/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Template10.Common;
+using Windows.ApplicationModel.Activation;
+
+namespace WindowsApp24II
+{
+    /// Documentation on APIs used in this page:
+    /// https://github.com/Windows-XAML/Template10/wiki
+    sealed partial class App : Template10.Common.BootStrapper
+    {
+        public App()
+        {
+            InitializeComponent();
+            SplashFactory = e => { return null; };
+        }
+
+        public override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+
+        //public override Task OnPrelaunchAsync(IActivatedEventArgs args, out bool runOnStartAsync)
+        //{
+        //    runOnStartAsync = false;
+        //    return Task.CompletedTask;
+        //}
+
+        public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            NavigationService.Navigate(typeof(Views.MainPage));
+            return Task.CompletedTask;
+        }
+
+    }
+
+
+}
