2876a99d4679293abdba99dfe0e3a845b9079592
Jose I. Padilla <jpadil02@baker.edu>
2019-04-14 21:48:58 -0400

    x bind update

---
+++ WindowsApp223/WindowsApp223/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using WindowsApp223.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace WindowsApp223.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
