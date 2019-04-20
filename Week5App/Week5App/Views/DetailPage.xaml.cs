6288a8d7394c1f7fab219e3c2f0571f03acdb0d6
Jose I. Padilla <jpadil02@baker.edu>
2019-04-20 19:16:53 -0400

    all 5 lessons added

---
+++ Week5App/Week5App/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Week5App.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Week5App.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
---
+++ WindowsApp23/WindowsApp23/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using WindowsApp23.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace WindowsApp23.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
