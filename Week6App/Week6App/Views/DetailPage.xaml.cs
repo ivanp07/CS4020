b435199491f813ee0437d0f7d8978b4c55e1e001
Jose I. Padilla <jpadil02@baker.edu>
2019-04-28 22:47:37 -0400

    added all from videos/PP

---
+++ Week6App/Week6App/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Week6App.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Week6App.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
